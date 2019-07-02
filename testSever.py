ff8fd6a046a48db68371b7fc3e709bc75b8702cd
Moulid Ahmed <moulidah@buffalo.edu>
2019-07-02 13:12:14 -0400

    Update testSever.py

--- testSever.py
+++ testSever.py
@@ -12,7 +12,7 @@ datastore = defaultdict(int)
 
 async def counter1(channel,count1):
     async for message in channel.history(limit=None):
-        if message.content.lower().find('nigger') != -1 or message.content.lower().find('nigga') != -1:
+        if message.content.lower().find('badword1') != -1 or message.content.lower().find('badword2') != -1:
             user = message.author.display_name
             if user in count1:
                 count1[user] += 1
@@ -65,16 +65,16 @@ async def on_message(message):
             for i in d.keys():
                 buffer1='|        '+ str(i)+ '    |        ' +'$'+str(d[i]*4) +  '     |'+'\n'
                 buffer2 += buffer1
-            await channel.send(" ``<-------------- N Word Score board ---------------->\n"+buffer2 +'``')
+            await channel.send(" ``<-------------- bad Word Score board ---------------->\n"+buffer2 +'``')
         else:
             await channel.send('collecting data....')
         print("exiting !allncount.....")
     #     await counter2(message.channel)
 
     if message.content.lower().find('send1') != -1:
-        await message.channel.send('nigger')
+        await message.channel.send('badword')
 
-    if message.content.lower().find('nigger') != -1 or message.content.lower().find('nigga') != -1:
+    if message.content.lower().find('badword1') != -1 or message.content.lower().find('badword2') != -1:
         print('someone said the N word....')
         filename = 'jsonFile2.json'
         if os.path.exists(filename):
