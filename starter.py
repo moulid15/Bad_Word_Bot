d27850583e339d66e171d224bf59591e92270120
Moulid Ahmed <moulidah@buffalo.edu>
2019-07-02 20:18:26 -0400

    Update starter.py

--- starter.py
+++ starter.py
@@ -2,7 +2,7 @@ import discord
 import json
 from json.decoder import JSONDecodeError
 
-token = 'NTkzOTM1MzQxOTA0MzMwNzU0.XRjAzA.2_cgw5ie7VYB5zdfj-nU1BmZ86Y'
+token = 
 
 count = dict = {}
 client = discord.Client()
@@ -11,7 +11,7 @@ count1=dict={}
 
 async def counter1(channel):
     async for message in channel.history(limit=None):
-        if message.content.lower().find('nigger') != -1 or message.content.lower().find('nigga') != -1:
+        if message.content.lower().find('badword1') != -1 or message.content.lower().find('badword') != -1:
             user = message.author.display_name
             if user in count1:
                 count1[user] += 1
@@ -22,7 +22,7 @@ async def counter1(channel):
 async def counter2(channel):
     buffer2=''
     async for message in channel.history(limit=None):
-        if message.content.lower().find('nigger') != -1 or message.content.lower().find('nigga') != -1:
+        if message.content.lower().find('badword1') != -1 or message.content.lower().find('badword') != -1:
             user = message.author.display_name
             # print(type(user))
             if user in count1:
@@ -33,7 +33,7 @@ async def counter2(channel):
     for i in count1.keys():
         buffer1='|        '+ str(i)+ '    |        ' +'$'+str(count1[i]*4) +  '     |'+'\n'
         buffer2 += buffer1
-    await channel.send(" ``<-------------- N Word Score board ---------------->\n"+buffer2 +'``')
+    await channel.send(" ``<-------------- badword Word Score board ---------------->\n"+buffer2 +'``')
 
 
 @client.event
@@ -53,7 +53,7 @@ async def on_message(message):
         count1.clear()
 
     if message.content.lower().find('send1') != -1:
-        await message.channel.send('nigger')
+        await message.channel.send('badword')
 
 # print(counter)
 client.run(token)
